5aedf4d24851b1ba84210574036cf72121e614c4
Dgiannaris <mtsgiannaris@gmail.com>
2019-06-24 18:08:14 +0300

    microposts URLs & introduction in validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
  
+  resources :microposts
   resources :users
  Rails.application.routes.draw do
+  resources :microposts
   resources :users
   root 'application#hello'
 
