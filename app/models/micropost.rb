ab161e3000e955ccda12e580f14f4f571626e8c8
Dgiannaris <mtsgiannaris@gmail.com>
2019-06-25 15:28:35 +0300

    Finishing toy_app

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,7 @@
 class Micropost < ApplicationRecord
 
 belongs_to :user
-validates :content, length: { maximum: 140 },
-#excercises 2.3
+validates :content, length: { maximum: 140 },presence: true
+
 
 end
